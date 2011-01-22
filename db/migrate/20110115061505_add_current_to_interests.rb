14f14b1fe7adbfa8a9e30ff73589ea1b29ffcb45
Kevin Pope <pope.kevin@gmail.com>
2011-01-22 14:18:10 +1100

    changed db structure to include user_id2

--- db/migrate/20110115061505_add_current_to_interests.rb
+++
@@ -1,9 +0,0 @@
-class AddCurrentToInterests < ActiveRecord::Migration
-  def self.up
-    add_column :interests, :current, :boolean
-  end
-
-  def self.down
-    remove_column :interests, :current
-  end
-end
