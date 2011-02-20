b9b555471bfcbbf018c387f1e35938307ce58622
Kevin Pope <pope.kevin@gmail.com>
2011-02-20 19:18:03 +1100

    updated controllers and views

--- app/models/autoscale.rb
+++ app/models/autoscale.rb
@@ -82,25 +82,5 @@ class Autoscale
         e.destroy 
       end
     end
-      
-#    DelayedJob.handle_asynchronously :testerama, :run_at => Proc.new {5.minutes.from_now}
-
-=begin
-  
-  Order of operations:
-  1) Check for number of dynos.  
-    1a) If 0, set to 1 and return.  
-    1b) If 1, remove the 'shut down the worker' thread from the delayed job table
-  2) check wait time, if more than 10 seconds, spawn another worker (but comment this bit out for now)
-  3) check the queue length, and if it is 0, set the death time for the worker in the delayed job table.
-  
-  --- !ruby/struct:Delayed::PerformableMethod 
-  object: !ruby/class Autoscale
-  method_name: :delayed_worker_close
-  args: []
-
-  
-  
-=end
 
 end
