213fd200338fb0b8138ee67f51c2a2be47d68a43
Kevin Pope <pope.kevin@gmail.com>
2011-02-20 17:08:58 +1100

    check

--- db/migrate/20110217100415_add_email_me_to_users.rb
+++ db/migrate/20110217100415_add_email_me_to_users.rb
@@ -1,6 +1,6 @@
 class AddEmailMeToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :email_me, :boolean, :default => 1
+    add_column :users, :email_me, :boolean, :default => true
   end
 
   def self.down
