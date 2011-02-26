55f826aabf94b56253edc7bec135545fceef0dac
Kevin Pope <pope.kevin@gmail.com>
2011-02-26 20:40:53 +1100

    fixed order_by updated_at

--- app/controllers/defriend_controller.rb
+++ app/controllers/defriend_controller.rb
@@ -5,7 +5,7 @@ class DefriendController < ApplicationController
 		
     if session[:user_id]
       if (session[:expire] > Time.now)
-        @connections = Connection.where(:user_id => session[:user_id], :last_action => ['Defriended', 'Refriended', 'Canceled Account or Changed Privacy Settings', 'Reactivated Account', 'New Connection'], :updated_at => 30.days.ago.to_date..1.day.from_now.to_date).all
+        @connections = Connection.where(:user_id => session[:user_id], :last_action => ['Defriended', 'Refriended', 'Canceled Account or Changed Privacy Settings', 'Reactivated Account', 'New Connection'], :updated_at => 30.days.ago.to_date..1.day.from_now.to_date).order('updated_at DESC').all
       else
         session[:user_id] = nil
         flash[:notice] = "You have been logged out because it's been more than 24 hours since you last logged in"
