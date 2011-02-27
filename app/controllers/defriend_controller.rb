adff5645bac7065d0a0b7e78d08073a0861e03da
Kevin Pope <pope.kevin@gmail.com>
2011-02-27 13:21:19 +1100

    took out reactived bug fix code

--- app/controllers/defriend_controller.rb
+++ app/controllers/defriend_controller.rb
@@ -5,7 +5,6 @@ class DefriendController < ApplicationController
 		
     if session[:user_id]
       if (session[:expire] > Time.now)
-        Connection.correct_reactived
         @connections = Connection.where(:user_id => session[:user_id], :last_action => ['Defriended', 'Refriended', 'Canceled Account or Changed Privacy Settings', 'Reactivated Account', 'New Connection'], :updated_at => 30.days.ago.to_date..1.day.from_now.to_date).order('updated_at DESC').all
       else
         session[:user_id] = nil
